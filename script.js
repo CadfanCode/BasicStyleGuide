e0de458b27ea2d01c89d4e770325344ace90cae0
Cai <caiowain@gmail.com>
2025-11-19 12:32:12 +0100

    update js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const links = document.querySelectorAll('nav a');
 const content = document.getElementById('content');
 
 links.forEach(link => {
-    link.addEventListener('click', () => {
+    link.addEventListener('click', (e) => {
+         e.preventDefault();
         fetch(link.getAttribute('href'))
             .then(res => res.text())
             .then(html => {
