6fc3d970e7f5fe16939f426857b2a8fa0078de83
Cai <caiowain@gmail.com>
2025-11-19 13:03:01 +0100

    content added to rubriker page

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const content = document.getElementById('content');
 
 links.forEach(link => {
     link.addEventListener('click', (e) => {
-         e.preventDefault();
+         e.preventDefault(); // <-- This is critical so that the entire page doesn't reload. Otherwise the sidebar navigation will fail to appear!
         fetch(link.getAttribute('href'))
             .then(res => res.text())
             .then(html => {
